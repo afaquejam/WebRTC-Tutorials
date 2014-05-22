d8525432ff1545e7255af78f4edfabcda9175b22
Afaque Hussain <afaquejam@gmail.com>
2014-05-22 21:52:02 +0300

    Showing the streaming video to the streamer.

---
+++ CThaQ/SignalingServer/server.js
@@ -0,0 +1,49 @@
+var app = require('http').createServer(handler),
+io = require('socket.io').listen(app),
+fs = require('fs');
+
+app.listen(2014);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+var isStreaming = false;
+
+io.sockets.on('connection', function(socket) {
+  console.log("Connection Established from some peer.");
+  socket.emit('streamingStatus', isStreaming);
+
+  socket.on('updateStreamingStatus', function(status) {
+    isStreaming = status;
+    console.log("Streaming status updated to: " + isStreaming);
+  });
+
+  socket.on('sendOfferToPeer', function(offerData) {
+    console.log("Received a request to send offer to a peer.");
+    io.sockets.emit('sendingOffer', offerData);
+  });
+
+  socket.on('sendAnswerToPeer', function(AnswerData) {
+    console.log("Received a request to send Answer to the peer.");
+    io.sockets.emit('sendingAnswer', AnswerData);
+  });
+
+  socket.on('hangUpCall', function(data) {
+    console.log("Hanging up call.");
+    io.sockets.emit('closeCall', data);
+  });
+
+});
+
+
+
---
+++ CThaQ/WebApp/js/webrtc.js
@@ -0,0 +1,223 @@
+var socket = io.connect('http://localhost:2014');
+
+var localStream;
+var localPeerConnection;
+var remotePeerConnection;
+var offerData;
+var isReceiver;
+
+var constraints = {
+  video: true,
+  audio: false
+};
+
+var videoElement = document.getElementById("videoElement");
+var startButton = document.getElementById("startButton");
+var stopButton = document.getElementById("stopButton");
+
+startButton.onclick = startStreaming;
+stopButton.onclick = stopStreaming;
+
+startButton.disabled = true;
+stopButton.disabled = true;
+
+function startStreaming() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  getLocalStream();
+
+  socket.emit('updateStreamingStatus', true);
+  console.log("Started streaming.");
+}
+
+function stopStreaming() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  socket.emit('updateStreamingStatus', false);
+}
+
+socket.on('streamingStatus', function (isStreaming) {
+  if (!isStreaming) {
+    isReceiver = false;
+    startButton.disabled = false;
+
+    console.log("I will be streaming.");
+  } else {
+    isReceiver = true;
+    console.log("I will be receiving.");
+  }
+
+});
+
+
+
+// hangupButton.disabled = true;
+// acceptButton.disabled = true;
+
+// callButton.onclick = initiate;
+// hangupButton.onclick = hangup;
+// acceptButton.onclick = acceptCall;
+
+// function trace(text) {
+//   console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+// }
+
+function gotLocalPeerLocalStream(stream){
+  console.log("Got Local Media Stream.");
+  videoElement.src = URL.createObjectURL(stream);
+  localStream = stream;
+}
+
+function getLocalStream() {
+   navigator.webkitGetUserMedia(constraints, gotLocalPeerLocalStream,
+     errorCallback);
+}
+
+// function call() {
+//   isCaller = true;
+//   trace("Starting call");
+
+//   var servers = null;
+
+//   localPeerConnection = new webkitRTCPeerConnection(servers);
+
+//   localPeerConnection.addStream(localStream);
+//   trace("Added localStream to localPeerConnection");
+
+//   // createOffer triggers the ICE candidate gathering process at the local side.
+//   localPeerConnection.createOffer(gotLocalDescription);
+//   localPeerConnection.onicecandidate = gotLocalIceCandidate;
+//   localPeerConnection.onaddstream = gotRemoteStream;
+// }
+
+// function gotLocalDescription(description){
+//   // The Generated SDP does not contain ICE candidates.
+//   localPeerConnection.setLocalDescription(description);
+//   trace("Local Description Set.");
+// }
+
+// function gotLocalIceCandidate(event) {
+//   if (event.target.iceGatheringState == "complete") {
+//     trace("Local ICE candidates gathering complete.");
+
+//     // Once the ICE gathering state is completed, create an SDP updated with
+//     // the ICE candidates.
+//     localPeerConnection.createOffer(sendOffer);
+//   }
+// }
+
+// function sendOffer(description) {
+//   trace("Sending offer to the other peer.");
+//   socket.emit('sendOfferToPeer', description);
+// }
+
+// socket.on('sendingOffer', function (offer) {
+//   if (!isCaller) {
+//     acceptButton.disabled = false;
+//     hangupButton.disabled = false;
+//     callButton.disabled = true;
+//     trace('Incoming Call.');
+
+//     offerData = new RTCSessionDescription(offer);
+//   }
+// });
+
+// function acceptCall() {
+//   acceptCallButton.disabled = true;
+//   var servers = null;
+
+//   remotePeerConnection = new webkitRTCPeerConnection(servers);
+
+//   remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
+//   remotePeerConnection.onaddstream = gotRemoteStream;
+
+//   remotePeerConnection.setRemoteDescription(offerData);
+//   trace("Remote description of Remote peer set.");
+
+//   navigator.webkitGetUserMedia(constraints, gotRemotePeerLocalStream,
+//     errorCallback);
+
+// }
+
+// function gotRemotePeerLocalStream(stream){
+//   callButton.disabled = true;
+
+//   localVideo.src = URL.createObjectURL(stream);
+//   remotePeerConnection.addStream(stream);
+
+//   //Create Answer triggers the ICE gathering process at the remote peer.
+//   remotePeerConnection.createAnswer(gotRemoteDescription);
+// }
+
+// function gotRemoteDescription(description){
+//   // The remote peer also sets its local description without the ICE candidates.
+//   remotePeerConnection.setLocalDescription(description);
+//   trace("Local description of the remote peer set.");
+// }
+
+// function gotRemoteIceCandidate(event){
+//   if (event.target.iceGatheringState == "complete") {
+//     trace("Remote ICE candidates gathering complete.");
+
+//     // Once the ICE gathering state is completed, create an SDP updated with
+//     // the ICE candidates.
+//     remotePeerConnection.createAnswer(sendAnswer);
+//   }
+// }
+
+// function sendAnswer(description) {
+//   trace("Sending Answer to the other peer.");
+//   socket.emit('sendAnswerToPeer', description);
+// }
+
+// socket.on('sendingAnswer', function (AnswerData) {
+//   if (isCaller) {
+//     trace("Answer received from the remote peer.");
+//     // For some reason, socket.io doesn't preserve the object type.
+//     AnswerData = new RTCSessionDescription(AnswerData);
+
+//     localPeerConnection.setRemoteDescription(AnswerData);
+//     trace("Remote description of the local peer set.");
+//   }
+
+// });
+
+// function gotRemoteStream(event){
+//   remoteVideo.src = URL.createObjectURL(event.stream);
+//   trace("Received remote stream.");
+// }
+
+// function hangup() {
+//   socket.emit('hangUpCall', null);
+// }
+
+// socket.on('closeCall', function (data) {
+//   if (isCaller) {
+//     localPeerConnection.close();
+//     localPeerConnection = null;
+//     localStream = null;
+//     offerData = null;
+//     isCaller = false;
+//   } else {
+//     if(remotePeerConnection) {
+//       remotePeerConnection.close();
+//       remotePeerConnection = null;
+//     }
+//   }
+
+//   callButton.disabled = false;
+//   hangupButton.disabled = true;
+//   acceptCallButton.disabled = true;
+
+//   localVideo.src = null;
+//   remoteVideo.src = null;
+
+//   trace("Call closed.");
+
+// });
+
+function errorCallback() {
+  console.log("Failed to get User Media!");
+  socket.emit('hangUpCall', null);
+}
+
